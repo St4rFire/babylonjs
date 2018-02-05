b0d8785febd87d61ba7285a1f0da6cdf0397c395
Flavio Peri <flavio.peri@openmindonline.it>
2018-02-05 17:31:08 +0100

    download texture by name

--- src/main/java/it/openmindonline/babylonjs/BabylonController.java
+++ src/main/java/it/openmindonline/babylonjs/BabylonController.java
@@ -1,9 +1,19 @@
 package it.openmindonline.babylonjs;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,5 +29,28 @@ public class BabylonController
         model.addAttribute("name", name);
         return "babylon";
     }
+
+    @RequestMapping(value = "/downloadtexture")
+    public void downloadOrder(@RequestParam(value="name") String name, HttpServletResponse response)
+    {
+
+        try (InputStream in = getClass().getResourceAsStream("/static/babylon/mesh/spaceship/0.png")) // todo use convention
+        {
+            response.setHeader("Content-Disposition", "attachment; filename=" + name + ".png");
+            response.setContentType("application/xml");
+            //response.setContentLength(in.length);
+            ServletOutputStream outputStream = response.getOutputStream();
+            IOUtils.copy(in, outputStream);
+            IOUtils.closeQuietly(outputStream);
+            IOUtils.closeQuietly(in);
+        }
+        catch (Exception e)
+        {
+            //log.error("Error exporting order {}", orderNumber, e);
+        }
+    }
+
+
+
 
 }
--- src/main/java/it/openmindonline/babylonjs/BabylonjsApplication.java
+++ src/main/java/it/openmindonline/babylonjs/BabylonjsApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class BabylonjsApplication
 {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BabylonjsApplication.class, args);
 	}
